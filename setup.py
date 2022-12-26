64df86ae41dbefc91151497e3a19e33b90cad7d1
Jaswanthravichandran <97626198+Jaswanthravichandran@users.noreply.github.com>
2022-12-26 16:11:01 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='NetRecon',
+      version='1.0',
+      description='Network Recon Tool',
+      url='https://github.com/Jaswanthravichandran/NetRecon.git',
+      author='Jaswanth',
+      author_linkedin='https://www.linkedin.com/in/jaswanth-r-32b1b1217/',
+      install_requires=['requests==2.21.0'],
+      python_requires=">=3",
+      zip_safe=False)
